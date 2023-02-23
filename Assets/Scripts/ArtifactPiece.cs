ac407286a0da1b32d674c2505d318f0d4ceaf829
Tevis Doe <doet@mcmaster.ca>
2023-02-23 15:11:58 -0500

    Update click logic to use tool enum

--- Assets/Scripts/ArtifactPiece.cs
+++ Assets/Scripts/ArtifactPiece.cs
@@ -8,8 +8,23 @@ public class ArtifactPiece : MonoBehaviour
     [SerializeField]
     private String message;
 
-    public void onClick()
+    public void noneClick()
     {
         Debug.Log(message);
+    }
+
+    public void magnifierClick()
+    {
+        Debug.Log("You used the magnifying glass.");
+    }
+
+    public void brushClick()
+    {
+        Debug.Log("You used the brush.");
+    }
+
+    public void materialClick()
+    {
+        Debug.Log("You used the material analyzer.");
     }
 }
--- Assets/Scripts/ClickLogic.cs
+++ Assets/Scripts/ClickLogic.cs
@@ -7,6 +7,20 @@ public class ClickLogic : MonoBehaviour
     [SerializeField]
     private LayerMask clickLayer;
 
+    private enum Tool
+    {
+        Magnifier,
+        Brush,
+        Material,
+        None
+    }
+    private Tool currentTool;
+
+    private void Start()
+    {
+        currentTool = Tool.None;
+    }
+
     private void Update()
     {
         if(Input.GetMouseButtonDown(0))
@@ -14,8 +28,29 @@ public class ClickLogic : MonoBehaviour
             RaycastHit rayHit;
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, 100f, clickLayer))
             {
-                rayHit.collider.GetComponent<ArtifactPiece>().onClick();
+                ArtifactPiece hitPiece = rayHit.collider.GetComponent<ArtifactPiece>(); 
+                if (currentTool == Tool.None) hitPiece.noneClick();
+                else if (currentTool == Tool.Magnifier) hitPiece.magnifierClick();
+                else if (currentTool == Tool.Brush) hitPiece.brushClick();
+                else hitPiece.materialClick();
             }
         }
+    }
+
+    public void toggleMagnifier()
+    {
+        if (currentTool != Tool.Magnifier) currentTool = Tool.Magnifier;
+        else currentTool = Tool.None;
+    }
+
+    public void toggleBrush()
+    {
+        if (currentTool != Tool.Brush) currentTool = Tool.Brush;
+        else currentTool = Tool.None;
+    }
+    public void toggleMaterial()
+    {
+        if (currentTool != Tool.Material) currentTool = Tool.Material;
+        else currentTool = Tool.None;
     }
 }
